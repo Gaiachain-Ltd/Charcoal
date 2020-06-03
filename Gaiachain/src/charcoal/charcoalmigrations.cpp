807f2e85f692069d4ee9f2b26f248bc2e15e6549
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-06-03 14:59:49 +0200

    Refs #85493. Preparing some static and dummy data for SQL.

--- Gaiachain/src/charcoal/charcoalmigrations.cpp
+++ Gaiachain/src/charcoal/charcoalmigrations.cpp
@@ -28,19 +28,52 @@ const QVector<Migration> db::DB_MIGRATIONS = {
             QLatin1String("CREATE TABLE Destinations (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
             QLatin1String("CREATE TABLE OvenTypes (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
             // SupplyChain
-QLatin1String("CREATE TABLE Plots (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
-QLatin1String("CREATE TABLE Harvests (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
-QLatin1String("CREATE TABLE Transports (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)")
+            QLatin1String("CREATE TABLE Transactions (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
+            QLatin1String("CREATE TABLE TransactionTypes (`id` INTEGER primary key UNIQUE, `name` TEXT NOT NULL)"),
+            QLatin1String("CREATE TABLE Steps (`id` INTEGER primary key UNIQUE, `date` INTEGER NOT NULL, `locationLat` REAL NOT NULL, `locationLon` REAL NOT NULL, `properties` TEXT NOT NULL)")
         }, true),
         std::bind(&Helpers::runQueries, std::placeholders::_1, QList<QLatin1String>{
+            // Additional data
             QLatin1String("DROP TABLE Villages"),
             QLatin1String("DROP TABLE TreeSpecies"),
             QLatin1String("DROP TABLE Parcels"),
             QLatin1String("DROP TABLE Destinations"),
             QLatin1String("DROP TABLE OvenTypes"),
-            QLatin1String("DROP TABLE Plots"),
-            QLatin1String("DROP TABLE Harvests"),
-            QLatin1String("DROP TABLE Transports"),
+            // Supply chain
+            QLatin1String("DROP TABLE Transactions"),
+            QLatin1String("DROP TABLE TransactionTypes"),
+            QLatin1String("DROP TABLE Steps"),
+        }, true)
+    },
+    // This inserts some static data!
+    {
+        { 0, 0, 3 },
+        std::bind(&Helpers::runQueries, std::placeholders::_1, QList<QLatin1String>{
+            // Additional data
+            // Nothing static here
+            // Supply chain
+            QLatin1String("INSERT INTO TransactionTypes (name) VALUES (`Plot`)"),
+            QLatin1String("INSERT INTO TransactionTypes (name) VALUES (`Harvest`)"),
+            QLatin1String("INSERT INTO TransactionTypes (name) VALUES (`Transport`)"),
+        }, true),
+        std::bind(&Helpers::runQueries, std::placeholders::_1, QList<QLatin1String>{
+            QLatin1String()
+        }, true)
+    },
+    // This inserts some DUMMY data!
+    // TODO: REMOVE - use web to get this info
+    {
+        { 0, 0, 4 },
+        std::bind(&Helpers::runQueries, std::placeholders::_1, QList<QLatin1String>{
+            // Additional data
+            QLatin1String("INSERT INTO Villages (name) VALUES (`Village One`)"),
+            QLatin1String("INSERT INTO Villages (name) VALUES (`Village Two`)"),
+            // Supply chain
+            QLatin1String("INSERT INTO TreeSpecies (name) VALUES (`Cassia siamea`)"),
+            QLatin1String("INSERT INTO TreeSpecies (name) VALUES (`Technona grandis`)"),
+        }, true),
+        std::bind(&Helpers::runQueries, std::placeholders::_1, QList<QLatin1String>{
+            QLatin1String()
         }, true)
     },
 };
