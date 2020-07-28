6062783a5c7e7bed56d74915aa6f897b8f2d0bdf
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-07-28 12:52:04 +0200

    R#87992. Do not show plot ID as available if logging ending event is already registered.

--- Gaiachain/src/charcoal/models/unusedplotidsmodel.cpp
+++ Gaiachain/src/charcoal/models/unusedplotidsmodel.cpp
@@ -8,5 +8,7 @@ UnusedPlotIdsModel::UnusedPlotIdsModel(QObject *parent) : SimpleListQueryModel(p
 {
     setWebModelCanChange(true);
     setDbQuery("SELECT id, name FROM Entities WHERE isFinished=0 "
-               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\")");
+               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") "
+               "AND (SELECT (COUNT(DISTINCT id) < 2) FROM Events "
+               "WHERE entityId=Entities.id)");
 }
