9e5cbd959eb4a450bb275a76a2a9da87553f2c69
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-07-29 17:31:41 +0200

    Fix carbonization beginning error.

--- Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
+++ Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
@@ -4,10 +4,10 @@ UnusedPlotIdsForCarbonizationModel::UnusedPlotIdsForCarbonizationModel(QObject *
     : SimpleListQueryModel(false, parent)
 {
     setWebModelCanChange(true);
-    setDbQuery("SELECT id, name FROM Entities WHERE isFinished=0 "
-               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") "
-               "AND SELECT (COUNT(DISTINCT id) = 2) FROM Events "
-               "WHERE entityId=Entities.id)");
+    setDbQuery("SELECT id, name FROM Entities e WHERE isFinished=0 "
+               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") ");
+               //"AND SELECT COUNT(DISTINCT id) = 2 FROM Events "
+               //"WHERE entityId=e.id)");
     // Query does ONLY show Entities where 2 events are already defined (both
     // logging beginning and logging ending). Only such plots can be carbonized
 }
