619ad17e4d615ba23d28e1b914bf946ced6f927c
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-07-29 09:48:30 +0200

    R#87992. Only allow carbonization of plots where both LB and LE events are present.

--- Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
+++ Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
@@ -5,5 +5,9 @@ UnusedPlotIdsForCarbonizationModel::UnusedPlotIdsForCarbonizationModel(QObject *
 {
     setWebModelCanChange(true);
     setDbQuery("SELECT id, name FROM Entities WHERE isFinished=0 "
-               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\")");
+               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") "
+               "AND SELECT (COUNT(DISTINCT id) = 2) FROM Events "
+               "WHERE entityId=Entities.id)");
+    // Query does ONLY show Entities where 2 events are already defined (both
+    // logging beginning and logging ending). Only such plots can be carbonized
 }
--- Gaiachain/src/charcoal/models/unusedplotidsforloggingendingmodel.cpp
+++ Gaiachain/src/charcoal/models/unusedplotidsforloggingendingmodel.cpp
@@ -8,4 +8,6 @@ UnusedPlotIdsForLoggingEndingModel::UnusedPlotIdsForLoggingEndingModel(QObject *
                "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") "
                "AND (SELECT (COUNT(DISTINCT id) < 2) FROM Events "
                "WHERE entityId=Entities.id)");
+    // Query does not show Entities where 2 events are already defined (both
+    // logging beginning and logging ending).
 }
