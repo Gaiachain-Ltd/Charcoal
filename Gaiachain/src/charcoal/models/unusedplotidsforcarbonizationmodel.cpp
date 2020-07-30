bf1eeb436c653c9b2dd8ee4515e6e79c0dbbe40b
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-07-30 07:59:58 +0200

    Fix query for looking for plots valid for carbonization beginning.

--- Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
+++ Gaiachain/src/charcoal/models/unusedplotidsforcarbonizationmodel.cpp
@@ -5,9 +5,9 @@ UnusedPlotIdsForCarbonizationModel::UnusedPlotIdsForCarbonizationModel(QObject *
 {
     setWebModelCanChange(true);
     setDbQuery("SELECT id, name FROM Entities e WHERE isFinished=0 "
-               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") ");
-               //"AND SELECT COUNT(DISTINCT id) = 2 FROM Events "
-               //"WHERE entityId=e.id)");
+               "AND typeId IN (SELECT id FROM EntityTypes WHERE name=\"Plot\") "
+               "AND (SELECT COUNT(DISTINCT id) FROM Events "
+               "WHERE entityId=e.id)=2");
     // Query does ONLY show Entities where 2 events are already defined (both
     // logging beginning and logging ending). Only such plots can be carbonized
 }
